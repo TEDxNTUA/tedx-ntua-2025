24e73cd19ea34ccf0643b1c9dc8dc9fa9b1848a4
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-02-23 18:43:57 +0200

    feat: add initial footer with page links
    - Implemented a non-final footer structure
    - Included links to other pages for navigation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,43 @@
-function Footer() {
-  return <div>Test Footer</div>;
-}
+"use client";
 
-export default Footer;
+import Link from "next/link";
+import SocialMediaFooter from "./SocialMediaFooter";
+
+import styles from "./magnetic.module.css";
+
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white p-6 md:p-12">
+      {/* Nav links*/}
+      <ul className="col-span-1 grid grid-cols-2 grid-rows-2 z-10 mt-4 lg:text-2xl justify-center justify-items-center">
+        <li>
+          <Link href="/" className="text-white text-md font-bold">
+            <p className={styles.magnetic}>Home</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/speakers" className="text-white text-md font-bold">
+            <p className={styles.magnetic}>Speakers</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/partners" className="text-white text-md font-bold">
+            <p className={styles.magnetic}>Sponsors</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about" className="text-white text-md font-bold">
+            <p className={styles.magnetic}>About</p>
+          </Link>
+        </li>
+      </ul>
+
+      <div className="flex flex-row justify-between bg-black">
+        <p className="text-center text-gray-500 text-xs">
+          ALL RIGHTS RESERVED © 2025
+        </p>
+        <SocialMediaFooter />
+      </div>
+    </footer>
+  );
+}
