5ec38beb5f83d81f229017d1617fcf997f12c926
Korina Karatzouni <st2yp01@gmail.com>
2025-03-21 16:50:26 +0200

    Added Synelixis moving logo at main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,59 @@
+'use client'
+
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import styles from './page.module.css';
+
 export default function Home() {
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.5,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+    });
+
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    if (xPercent < -100) {
+      xPercent = 0;
+    } else if (xPercent > 0) {
+      xPercent = -100;
+    }
+
+    gsap.set(slider.current, { xPercent: xPercent });
+
+    xPercent += 0.1 * direction;
+    requestAnimationFrame(animate);
+  };
+
   return (
-    <main>
-      <p>TEDxNTUA 2025 main page</p>
+    <main className={styles.main}>
+      {/* Scrolling Text at the Top */}
+      <div className={styles.sliderContainer}>
+        <div ref={slider} className={styles.slider}>
+          {Array(20).fill('Synelixis').map((word, index) => (
+            <span key={index}>{word}</span>
+          ))}
+        </div>
+      </div>
+
+      {/* Rest of the content below */}
+      <div>
+        <p>TEDxNTUA 2025 main page</p>
+      </div>
     </main>
   );
 }
