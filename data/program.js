07edab99ae7f953a6166f4741652bf45cee8a98e
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-03-30 20:09:40 +0300

    feat: add dummy schedule/programm data

---
+++ data/program.js
@@ -0,0 +1,133 @@
+// Room Mapping:
+// Main Stage = 0
+// Room 1 = 1
+// Room 2 = 2
+// Room 3 = 3
+
+// Happening Type
+// 0 = Talk
+// 1 = Performance
+// 2 = Workshop
+
+export const PROGRAM = [
+  {
+    hour: '11:00',
+    sessions: [
+      {
+        type: 0,
+        title: 'Talk 1',
+        speakers: ['Speaker 1'],
+        room: 0,
+        description: 'Description of Talk 1',
+        image: 'https://example.com/image1.jpg'
+      },
+      {
+        type: 1,
+        title: 'Performance 2',
+        speakers: ['Speaker 2'],
+        room: 1,
+        description: 'Description of Talk 2',
+        image: 'https://example.com/image2.jpg'
+      },
+      {
+        type: 2,
+        title: 'Workshop 3',
+        speakers: ['Speaker 2'],
+        room: 2,
+        description: 'Description of Talk 2',
+        image: 'https://example.com/image2.jpg'
+      }
+    ]
+  },
+  {
+    hour: '12:00',
+    sessions: [
+      {
+        type: 0,
+        title: 'Talk 1',
+        speakers: ['Speaker 1'],
+        room: 0,
+        description: 'Description of Talk 1',
+        image: 'https://example.com/image1.jpg'
+      }
+    ]
+  },
+  {
+    hour: '12:20',
+    sessions: [
+      {
+        type: 0,
+        title: 'Talk 1',
+        speakers: ['Speaker 1'],
+        room: 0,
+        description: 'Description of Talk 1',
+        image: 'https://example.com/image1.jpg'
+      }
+    ]
+  },
+  {
+    hour: '12:50',
+    sessions: [
+      {
+        type: 0,
+        title: 'Talk 1',
+        speakers: ['Speaker 1'],
+        room: 0,
+        description: 'Description of Talk 1',
+        image: 'https://example.com/image1.jpg'
+      }
+    ]
+  },
+  {
+    hour: '13:15',
+    sessions: [
+      {
+        type: 0,
+        title: 'Talk 1',
+        speakers: ['Speaker 1'],
+        room: 0,
+        description: 'Description of Talk 1',
+        image: 'https://example.com/image1.jpg'
+      }
+    ]
+  },
+  {
+    hour: '13:35',
+    sessions: [
+      {
+        type: 0,
+        title: 'Talk 1',
+        speakers: ['Speaker 1'],
+        room: 0,
+        description: 'Description of Talk 1',
+        image: 'https://example.com/image1.jpg'
+      }
+    ]
+  },
+  {
+    hour: '13:55',
+    sessions: [
+      {
+        type: 0,
+        title: 'Talk 1',
+        speakers: ['Speaker 1'],
+        room: 0,
+        description: 'Description of Talk 1',
+        image: 'https://example.com/image1.jpg'
+      }
+    ]
+  },
+  {
+    hour: '14:15',
+    sessions: [
+      {
+        type: 0,
+        title: 'Talk 1',
+        speakers: ['Speaker 1'],
+        room: 0,
+        description: 'Description of Talk 1',
+        image: 'https://example.com/image1.jpg'
+      }
+    ]
+  }
+];
