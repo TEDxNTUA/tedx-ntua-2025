b84b462afd8fffd41d2971b3f840cb6d596fa22f
Vivian Thanou <el20225@mail.ntua.gr>
2025-04-02 17:40:25 +0300

    components added + building meet the team began

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,255 +1,52 @@
 'use client';
-import {useState} from 'react';
+import { useState } from 'react';
 import styles from './page.module.scss';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import useMousePosition from '../utils/useMousePosition';
+import AboutInfo from '../../components/about/AboutInfo';
+import MeetTheTeam from "../../components/about/MeetTheTeam";
+import AboutGrid from "../../components/about/AboutGrid";
+import { ABOUTLIST } from "../../../data/about";
 
 export default function AboutPage() {
   const [isHovered, setIsHovered] = useState(false);
-  const {x, y} = useMousePosition();
+  const { x, y } = useMousePosition();
   const size = isHovered ? 100 : 30;
 
   return (
     <div>
+      {/* Main includes information about TED, TEDx and TEDxNTUA with a mask interactive effect */}
       <main className={styles.main}>
         {/* Here appears the text when it is hovered. */}
         <motion.div
           className={styles.mask}
           animate={{
-            WebkitMaskPosition: `${x - size / 2}px ${y - 120 - size / 2}px`,
+            WebkitMaskPosition: `${x - size / 2}px ${y - 65 - size / 2}px`,
             WebkitMaskSize: `${size}px`
           }}
-          transition={{type: 'tween', ease: 'backOut', duration: 0.5}}
+          transition={{ type: 'tween', ease: 'backOut', duration: 0.5 }}
         >
           <div
-            onMouseEnter={() => {
-              setIsHovered(true);
-            }}
-            onMouseLeave={() => {
-              setIsHovered(false);
-            }}
+            onMouseEnter={() => { setIsHovered(true); }}
+            onMouseLeave={() => { setIsHovered(false); }}
             className={isHovered ? 'text-red' : 'text-white'}
           >
-            <div className="about">
-              <div className="flex flex-col lg:w-[80%] mx-auto">
-                {/* TED and TEDx side by side */}
-                <div className="flex flex-col lg:flex-row lg:justify-between gap-[4rem] text-justify lg:text-left">
-                  {/* TED Section */}
-                  <div className="left-col lg:w-[50%] w-[80%] mx-auto">
-                    <div className="title-wrapper mb-[2rem]">
-                      <h2 className="text-[2.25rem] lg:text-[4.25rem] font-bold">
-                        About <span className="text-white">TED</span>
-                      </h2>
-                    </div>
-                    <p className="text-[1rem] leading-[1.875rem] 4xl:text-[1.3rem] 4xl:leading-[2rem]">
-                      TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a
-                      four-day conference in California 30 years ago, TED has grown to support its
-                      mission with multiple initiatives. The two annual TED Conferences invite the
-                      world's leading thinkers and doers to speak for 18 minutes or less. Many of
-                      these talks are then made available, free, at TED.com. TED speakers have
-                      included Bill Gates, Jane Goodall, Elizabeth Gilbert, Sir Richard Branson,
-                      Nandan Nilekani, Philippe Starck, Ngozi Okonjo-Iweala, Sal Khan and Daniel
-                      Kahneman.
-                      <br />
-                      <br />
-                      The annual TED Conference takes place each spring in Vancouver, British
-                      Columbia. TED's media initiatives include TED.com, where new TED Talks are
-                      posted daily; TED Translators, which provides subtitles and interactive
-                      transcripts as well as translations from volunteers worldwide; the educational
-                      initiative TED-Ed. TED has established The Audacious Project that takes a
-                      collaborative approach to funding ideas with the potential to create change at
-                      thrilling scale; TEDx, which supports individuals or groups in hosting local,
-                      self-organized TED-style events around the world, and the TED Fellows program,
-                      helping world-changing innovators from around the globe to amplify the impact
-                      of their remarkable projects and activities.
-                    </p>
-                    <div className="mt-6 text-sm">
-                      Follow TED on{' '}
-                      <a
-                        href="https://twitter.com/TEDTalks"
-                        target="_blank"
-                        className="font-bold underline"
-                      >
-                        Twitter
-                      </a>{' '}
-                      or on{' '}
-                      <a
-                        href="https://www.facebook.com/TED"
-                        target="_blank"
-                        className="font-bold underline"
-                      >
-                        Facebook
-                      </a>
-                    </div>
-                  </div>
-
-                  {/* TEDx Section */}
-                  <div className="right-col lg:w-[50%] w-[80%] mx-auto">
-                    <div className="title-wrapper mb-[2rem]">
-                      <h2 className="text-[2.25rem] lg:text-[4.25rem] font-bold">
-                        About <span className="text-white">TEDx</span>
-                      </h2>
-                    </div>
-                    <p className="text-[1rem] leading-[1.875rem] 4xl:text-[1.3rem] 4xl:leading-[2rem]">
-                      In the spirit of ideas worth spreading, TEDx is a program of local,
-                      self-organized events that bring people together to share a TED-like
-                      experience. At a TEDx event, TED Talks video and live speakers combine to
-                      spark deep discussion and connection. These local, self-organized events are
-                      branded TEDx, where x = independently organized TED event. The TED Conference
-                      provides general guidance for the TEDx program, but individual TEDx events are
-                      self-organized. (Subject to certain rules and regulations.)
-                    </p>
-                    <div className="mt-6"></div>
-                    <a
-                      target="_blank"
-                      href="https://www.ted.com/about/programs-initiatives/tedx-program"
-                      className="text-white font-extrabold flex flex-row w-fit text-xl hover:cursor-pointer"
-                    >
-                      <p className="pr-3 hover:pr-6">TEDx PROGRAM </p>
-                      <img
-                        className="my-auto hover:pl-3"
-                        src="https://uploads-ssl.webflow.com/63e118320fe0876684cc169c/63e57c17119d6d2dca8a9624_red-arrow.svg"
-                        style={{
-                          transform:
-                            'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
-                          transformStyle: 'preserve-3d'
-                        }}
-                        alt="Arrow"
-                      />
-                    </a>
-                  </div>
-                </div>
-
-                {/* TEDxNTUA Section (Below TED and TEDx) */}
-                <div className="w-[80%] lg:w-[50%] mx-auto mt-8">
-                  <div className="title-wrapper mb-[2rem]">
-                    <h2 className="text-[2.25rem] lg:text-[4.25rem] font-bold">
-                      About <span className="text-white">TEDxNTUA</span>
-                    </h2>
-                  </div>
-                  <p className="text-justify text-[1rem] leading-[1.875rem] 4xl:text-[1.3rem] 4xl:leading-[2rem]">
-                    TEDxNTUA is a conference that features talks on the topics of popular science,
-                    art, and social issues, stories that can inspire, as well as intriguing
-                    workshops and performances. It runs under the auspices of the National Technical
-                    University of Athens, Greece (NTUA) and is primarily aimed at the university
-                    community, but also at the wider student and local communities of Athens.
-                  </p>
-                </div>
-              </div>
-            </div>
+            <AboutInfo textColor="text-white" />
           </div>
         </motion.div>
         <div className={styles.body}>
           {/* Text without being hovered */}
-          <div className="about">
-            <div className="flex flex-col lg:w-[80%] mx-auto">
-              {/* TED and TEDx side by side */}
-              <div className="flex flex-col lg:flex-row lg:justify-between gap-[4rem] text-justify lg:text-left">
-                {/* TED Section */}
-                <div className="left-col lg:w-[50%] w-[80%] mx-auto">
-                  <div className="title-wrapper mb-[2rem]">
-                    <h2 className="text-[2.25rem] lg:text-[4.25rem] font-bold">
-                      About <span className="text-our-red">TED</span>
-                    </h2>
-                  </div>
-                  <p className="text-[1rem] leading-[1.875rem] 4xl:text-[1.3rem] 4xl:leading-[2rem]">
-                    TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a
-                    four-day conference in California 30 years ago, TED has grown to support its
-                    mission with multiple initiatives. The two annual TED Conferences invite the
-                    world's leading thinkers and doers to speak for 18 minutes or less. Many of
-                    these talks are then made available, free, at TED.com. TED speakers have
-                    included Bill Gates, Jane Goodall, Elizabeth Gilbert, Sir Richard Branson,
-                    Nandan Nilekani, Philippe Starck, Ngozi Okonjo-Iweala, Sal Khan and Daniel
-                    Kahneman.
-                    <br />
-                    <br />
-                    The annual TED Conference takes place each spring in Vancouver, British
-                    Columbia. TED's media initiatives include TED.com, where new TED Talks are
-                    posted daily; TED Translators, which provides subtitles and interactive
-                    transcripts as well as translations from volunteers worldwide; the educational
-                    initiative TED-Ed. TED has established The Audacious Project that takes a
-                    collaborative approach to funding ideas with the potential to create change at
-                    thrilling scale; TEDx, which supports individuals or groups in hosting local,
-                    self-organized TED-style events around the world, and the TED Fellows program,
-                    helping world-changing innovators from around the globe to amplify the impact of
-                    their remarkable projects and activities.
-                  </p>
-                  <div className="mt-6 text-sm">
-                    Follow TED on{' '}
-                    <a
-                      href="https://twitter.com/TEDTalks"
-                      target="_blank"
-                      className="font-bold underline"
-                    >
-                      Twitter
-                    </a>{' '}
-                    or on{' '}
-                    <a
-                      href="https://www.facebook.com/TED"
-                      target="_blank"
-                      className="font-bold underline"
-                    >
-                      Facebook
-                    </a>
-                  </div>
-                </div>
-
-                {/* TEDx Section */}
-                <div className="right-col lg:w-[50%] w-[80%] mx-auto">
-                  <div className="title-wrapper mb-[2rem]">
-                    <h2 className="text-[2.25rem] lg:text-[4.25rem] font-bold">
-                      About <span className="text-our-red">TEDx</span>
-                    </h2>
-                  </div>
-                  <p className="text-[1rem] leading-[1.875rem] 4xl:text-[1.3rem] 4xl:leading-[2rem]">
-                    In the spirit of ideas worth spreading, TEDx is a program of local,
-                    self-organized events that bring people together to share a TED-like experience.
-                    At a TEDx event, TED Talks video and live speakers combine to spark deep
-                    discussion and connection. These local, self-organized events are branded TEDx,
-                    where x = independently organized TED event. The TED Conference provides general
-                    guidance for the TEDx program, but individual TEDx events are self-organized.
-                    (Subject to certain rules and regulations.)
-                  </p>
-                  <div className="mt-6"></div>
-                  <a
-                    target="_blank"
-                    href="https://www.ted.com/about/programs-initiatives/tedx-program"
-                    className="text-our-red font-extrabold flex flex-row w-fit text-xl hover:cursor-pointer"
-                  >
-                    <p className="pr-3 hover:pr-6">TEDx PROGRAM </p>
-                    <img
-                      className="my-auto hover:pl-3"
-                      src="https://uploads-ssl.webflow.com/63e118320fe0876684cc169c/63e57c17119d6d2dca8a9624_red-arrow.svg"
-                      style={{
-                        transform:
-                          'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
-                        transformStyle: 'preserve-3d'
-                      }}
-                      alt="Arrow"
-                    />
-                  </a>
-                </div>
-              </div>
-
-              {/* TEDxNTUA Section (Below TED and TEDx) */}
-              <div className="w-[80%] lg:w-[50%] mx-auto mt-8">
-                <div className="title-wrapper mb-[2rem]">
-                  <h2 className="text-[2.25rem] lg:text-[4.25rem] font-bold">
-                    About <span className="text-our-red">TEDxNTUA</span>
-                  </h2>
-                </div>
-                <p className="text-justify text-[1rem] leading-[1.875rem] 4xl:text-[1.3rem] 4xl:leading-[2rem]">
-                  TEDxNTUA is a conference that features talks on the topics of popular science,
-                  art, and social issues, stories that can inspire, as well as intriguing workshops
-                  and performances. It runs under the auspices of the National Technical University
-                  of Athens, Greece (NTUA) and is primarily aimed at the university community, but
-                  also at the wider student and local communities of Athens.
-                </p>
-              </div>
-            </div>
-          </div>
+          <AboutInfo textColor="text-our-red" />
         </div>
       </main>
+
+      {/* This div includes the grid with the team members and their doodles */}
+      <div className="max-w-[2080px] mx-auto flex flex-col lg:w-[90%] w-[80%] px-6 mt-[150px] bg-[#160B23]">
+        <h2 className="text-white text-[2.25rem] lg:text-[4.25rem] font-bold">
+          Meet The Team <span className="text-our-red">2025</span>
+        </h2>
+        <AboutGrid aboutlist={ABOUTLIST} />
+      </div>
     </div>
   );
 }
---
+++ src/components/about/AboutGrid.js
@@ -0,0 +1,52 @@
+"use client";
+import MeetTheTeam from "./MeetTheTeam";
+
+export default function AboutGrid({ aboutlist }) {
+  const filterPartnersByType = (teamUrl) => {
+    return aboutlist.filter((about) => about.teamUrl === teamUrl);
+  };
+
+  const displayOrder = [
+    "./meetTheTeam/CURATOR/THE CURAToR.jpg",
+    "./meetTheTeam/IT/INFoRMATIoN & TECHNoloGY TEAM.jpg",
+    "./meetTheTeam/DESIGN TEAM/DESIGN TEAM.jpg",
+    "./meetTheTeam/FR/FUNDRAISING TEAM.jpg",
+    "./meetTheTeam/EXP/EXPERIENCE & WoRKSHoPs TEAM.jpg",
+    "./meetTheTeam/MEDIA/MEDIA & MARKETING TEAM.jpg",
+    "./meetTheTeam/VENUE/VENUE ANd PRoDUCTIoN TEAM.jpg",
+    "./meetTheTeam/SPEAKERS/SPEAKERS TEAM.jpg"
+  ];
+
+  return (
+    <div className="max-w-[2080px] relative mx-auto flex flex-col gap-6 lg:gap-12 pb-12 ">
+      {displayOrder.map(
+        (
+          teamUrl //map se kathe omada
+        ) => (
+          <div key={teamUrl}>
+            <img className="pb-10" src={teamUrl} />
+            <ul className="flex flex-row items-center gap-4 lg:gap-12 justify-center mx-auto flex-wrap w-[100%] md:w-[80%]">
+              {filterPartnersByType(teamUrl).map(
+                (
+                  about,
+                  index //kathe melos
+                ) => (
+                  <li
+                    className="h-[150px] w-[150px] md:h-[200px] md:w-[200px] lg:h-[250px] lg:w-[250px] 2xl:h-[300px] 2xl:w-[300px]"
+                    key={index}
+                  >
+                    <MeetTheTeam
+                      photo={about.photo}
+                      doodles={about.doodles}
+                      link={about.linkedin}
+                    />
+                  </li>
+                )
+              )}
+            </ul>
+          </div>
+        )
+      )}
+    </div>
+  );
+}
