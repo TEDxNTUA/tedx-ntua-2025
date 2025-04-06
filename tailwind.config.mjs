9868bc51de142a5f2127dd7d6d778e2f116fc787
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-04-06 21:29:37 +0300

    chore: add synelixis colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,28 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ['class'],
-    content: [
+  darkMode: ['class'],
+  content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			'tedx-red': '#eb0028',
-  			'our-red': '#e14640',
-  			'our-black': '#191617',
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        'tedx-red': '#eb0028',
+        'our-red': '#e14640',
+        'our-black': '#191617',
         background: 'var(--background)',
         foreground: 'var(--foreground)',
         'tedx-red': '#eb0028',
         'our-red': '#e14640',
         'our-black': '#191617',
         'synelixis-blue': '#001a32',
-        'synelixis-yellow': '#f1be3d',
-        'synelixis-orange': '#ed6d23'},
-		}
+        'synelixis-orange': '#ed6d23',
+        'synelixis-yellow': '#f1be3d'
+      }
+    }
   },
-  plugins: [require("tailwindcss-animate")]
+  plugins: [require('tailwindcss-animate')]
 };
