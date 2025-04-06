e755ac0a85181c0d0a7929c85f67cd9a27b30ec0
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-04-06 20:12:05 +0300

    chore: initialize types for speakers, performances, partnes and workshops and add examples

---
+++ data/speakers.js
@@ -0,0 +1,184 @@
+//type Speaker = {
+//   slug: string,
+//   fullNameGR: string,
+//   fullNameEN: string,
+//   artisticName: string,
+//   job: string,
+//   infoLinks: string[],
+//   photo: string,
+//   session: number,
+//   time: string,
+//   talkCategory: string,
+//   talkDescription: string,
+//   bioGR: string,
+//   bioEN: string
+// };
+
+export const SPEAKERS = [
+  {
+    slug: 'ioannis-pantazopoulos',
+    fullNameGR: 'Ιωάννης Πανταζόπουλος',
+    fullNameEN: 'Ioannis Pantazopoulos',
+    artisticName: '',
+    job: '"LiFO" Chief Editor - Podcast Creator',
+    infoLinks: {
+      instagram: 'https://www.instagram.com/johnpantazopoulos/',
+      facebook: 'https://www.facebook.com/j.pantazopoulos',
+      x: 'https://x.com/JPantazopoulos',
+      podcast: '',
+      lifo: [
+        'https://www.lifo.gr/authors/giannis-pantazopoylos',
+        'https://www.lifo.gr/podcasts/akou-tin-epistimi'
+      ],
+      spotify: 'https://open.spotify.com/show/23AO8QeHCFgvN7h89kleOo',
+      applePodcast: 'https://podcasts.apple.com/gr/podcast/άκου-την-επιστήμη/id1546432943'
+    },
+    photo: './speakers/ioannis-pantazopoulos.jpg',
+    session: 1,
+    time: '',
+    talkCategory: 'Science Communication',
+    talkDescription:
+      'Πώς θα άλλαζε η ζωή σου αν είχες την ευκαιρία να αλληλεπιδράσεις με μερικούς από τους πιο καταξιωμένους επιστήμονες της χώρας; Να δεις τον κόσμο μέσα από το πρίσμα της οπτικής τους και να νιώσεις το πανταχού παρόν μεγαλείο της επιστήμης να παίρνει ανθρώπινη υπόσταση; Μήπως το να ακούς την επιστήμη είναι ο δίαυλος για αυτή την αμφίδρομη σχέση, όπου άνθρωποι και ιδέες έρχονται στη δική τους "συνέλιξη";',
+    bioGR:
+      'Ο Γιάννης Πανταζόπουλος σπούδασε στο τμήμα Πολιτικής Επιστήμης και Ιστορίας του Παντείου Πανεπιστημίου. Διαθέτει πολυετή εργασιακή εμπειρία στον τομέα των ΜME, ενώ από το 2014 αποτελεί βασικό μέλος της δημοσιογραφικής ομάδας της «LiFO» , κατέχοντας, σήμερα, τη θέση του Αρχισυντάκτη. Έχει επιμεληθεί τεύχη ειδικού περιεχομένου της έντυπης LiFO κι έχει πάρει συνεντεύξεις από κορυφαίες προσωπικότητες της πολιτικής, οικονομικής και ακαδημαϊκής κοινότητας. Τέλος, παρουσιάζει τη σειρά Podcast «Άκου την Επιστήμη» στην οποία φιλοξενούνται διακεκριμένες προσωπικότητες του επιστημονικού κόσμου.',
+    bioEN: ''
+  },
+  {
+    slug: 'georgios-gougoulakis',
+    fullNameGR: 'Γεώργιος Γουγουλάκης',
+    fullNameEN: 'Georgios Gougoulakis',
+    artisticName: '',
+    job: 'Architect',
+    infoLinks: {
+      linkedIn: 'https://www.linkedin.com/in/georgios-gougoulakis-4200b742/',
+      instagram: [
+        'https://www.instagram.com/g_architects_',
+        'https://www.instagram.com/giorgio_el_greco'
+      ],
+      facebook: [
+        'https://www.facebook.com/profile.php?id=100009012744658',
+        'https://www.facebook.com/gougoulakisarchitects'
+      ],
+      website: 'https://garchitects.gr'
+    },
+    photo: './speakers/georgios-gougoulakis.jpg',
+    session: 0,
+    time: '',
+    talkCategory: '',
+    talkDescription:
+      'Τι ορίζει την αναπηρία; Είναι η σωματική διαφοροποίηση ή η συνέλιξη της με μια μη συμπεριληπτική κοινωνία; Πόσο διαφορετικά θα ήταν τα πράγματα αν υπήρχαν κατάλληλες υποδομές και μια κοινωνία συμπεριληπτική για όλους;  Ο Γιώργος Γουγουλάκης, αρχιτέκτων μηχανικός και χρήστης αμαξιδίου, διερευνά πώς η κοινωνία διαμορφώνει την εμπειρία της αναπηρίας και πώς μπορούμε να φτιάξουμε έναν κόσμο φιλόξενο για όλους.',
+    bioGR:
+      'Ο Γιώργος Γουγουλάκης αποφοίτησε από τη σχολή Αρχιτεκτόνων Μηχανικών του ΕΜΠ το 2008, πρώτος στο έτος του. Έκανε το μεταπτυχιακό του στο Politecnico di Milano, στο Master in Transportation & Automobile design, και την πρακτική του στη Maserati. Έχει εργαστεί στο σχεδιαστικό οίκο ""Torino Design"" και σαν βοηθός διδασκαλίας στο Politecnico di Milano. Από το 2015 εργάζεται ως αρχιτέκτονας μηχανικός, ενώ το 2021 ίδρυσε το δικό του αρχιτεκτονικό γραφείο  “Gougoulakis Architects & Associates” (G Architects). ',
+    bioEN: ''
+  },
+  {
+    slug: 'rosario-moscato',
+    fullNameGR: '',
+    fullNameEN: 'Rosario Moscato',
+    artisticName: '',
+    job: 'President @MediterranIA',
+    infoLinks: {linkedIn: 'https://www.linkedin.com/in/rosariomoscato/'},
+    photo: '',
+    session: 0,
+    time: '',
+    talkCategory: 'AI Ethics',
+    talkDescription: '',
+    bioGR: '',
+    bioEN:
+      "Rosario Moscato, an AI and data science expert with 25+ years of global experience, blends innovation with ethics. Holding degrees in engineering, software design, and science and faith, he explores technology's impact on humanity. As an author, speaker, and CTO, he inspires transformative ideas about AI's future."
+  },
+  {
+    slug: 'kimon-vogt',
+    fullNameGR: 'Kίμων Φογκντ',
+    fullNameEN: 'Kimon Vogt',
+    job: 'CEO & Co-Founder @Sporo Health',
+    infoLinks: {
+      instagram: 'https://www.instagram.com/kimonvogt/',
+      linkedIn: 'https://www.linkedin.com/in/kimon-aristotelis-vogt/'
+    },
+    photo: '',
+    session: 0,
+    time: '',
+    talkCategory: '',
+    talkDescription: '',
+    bioGR: '',
+    bioEN:
+      'Kimon Vogt is the CEO and Co-Founder of Sporo Health. He has a M.S. in AI & Data Science from the Harvard John A. Paulson School of Engineering and Applied Sciences. Kimon taught Deep Learning (AC209) and Systems Computational Science (AC207) at Harvard University and was also a research associate at Carnegie Mellon University, where he developed and patented AI algorithms in healthcare. During his internship at Fidelity Investments and his Microsoft collaboration, he led AI algorithm development projects.'
+  },
+  {
+    slug: 'xenia-mastoraki',
+    fullNameGR: 'Ξένια Μαστοράκη',
+    fullNameEN: 'Xenia Mastoraki',
+    artisticName: '',
+    job: 'Αρχιτέκτων Μηχανικός, Αρχιτέκτων Τοπίου, Ιδρύτρια LandmArch',
+    infoLinks: {
+      instagram: [
+        'https://www.instagram.com/landmarch/',
+        'https://www.instagram.com/xeniamastoraki/'
+      ],
+      facebook: 'https://www.facebook.com/landmarch.architecture/',
+      linkedIn: 'https://www.linkedin.com/in/xeniamastoraki/',
+      website: 'https://www.landmarch.gr'
+    },
+    photo: '',
+    session: 0,
+    time: '',
+    talkCategory: '',
+    talkDescription: '',
+    bioGR:
+      'Αρχιτέκτων και Αρχιτέκτων Τοπίου, προάγει την απλότητα και τη χωρική ευελιξία μέσω καινοτόμων, bottom-up συστημάτων. Επικεντρώνεται στην ανεξάρτητη έρευνα και τη διάδοση γνώσεων για τον σύγχρονο σχεδιασμό βιώσιμων δημόσιων χώρων. Μέσω των μελετών της, δημιουργεί πρακτικές που ενδυναμώνουν τις κοινότητες, διαμορφώνοντας πόλεις που είναι όχι μόνο προσαρμοστικές αλλά και ικανές να εξελίσσονται μέσα από προκλήσεις.',
+    bioEN: ''
+  },
+  {
+    slug: 'simakis-sarantis-and-simakis-vangelis',
+    fullNameGR: 'Σιμάκης Σαράντης & Σιμάκης Βαγγέλης',
+    fullNameEN: 'Simakis Sarantis & Simakis Vangelis',
+    artisticName: 'Backpack Brothers / Α.Τ.Α.Χ.Τ.Ο.Ι.',
+    job: 'Ιδρυτές BACKPACK BROTHERS A.M.K.E. / A.T.A.X.T.O.I.',
+    infoLinks: {
+      instragram: [
+        'https://www.instagram.com/backpackbrothers.gr/',
+        'https://www.instagram.com/backpackbrothers.gr/'
+      ]
+    },
+    photo: '',
+    session: 0,
+    time: '',
+    talkCategory: '',
+    talkDescription: '',
+    bioGR:
+      '(Σαράντης) Γεννήθηκε και μεγάλωσε στο Ηράκλειο Κρήτης, όπου και σπούδασε Λογιστική και Χρηματοοικονομικά.  Τα τελευταία δώδεκα χρόνια ασχολείται ενεργά με τον Εθελοντισμό και την Κοινωνική Προσφορά έχοντας ιδρύσει το Εθελοντικό Σωματείο Α.Τ.Α.Χ.Τ.Ο.Ι. ( Άνθρωποι Της Αλληλεγγύης Χωρίς Τείχη & Οικονομική Ιδιοτέλεια), με μεγάλο κοινωνικό έργο στον Νομό Ηρακλείου. Παράλληλα τα τελευταία 4χρόνια έχουν ιδρύσει με τον αδερφό του Βαγγέλη τους Backpack Brothers με κύριο στόχο την βελτίωση συνθηκών σε χώρες της Αφρικής, της Λατινικής Αμερικής και της Ασίας. & (Βαγγέλης) Γεννήθηκε στο Ηράκλειο Κρήτης, ενώ σπούδασε Οικονομικές Επιστήμες στο Πανεπιστήμιο στο Ρέθυμνο. Κατέχει επίσης μεταπτυχιακό στην Εκπαίδευση και πιο συγκεκριμένα της Κριτικής Παιδαγωγικής και Εκπαίδευσης Εκπαιδευτικών του ίδιου ιδρύματος. Η ενασχόληση με τον εθελοντισμό ξεκίνησε στο Ηράκλειο και μέσα απο το Εθελοντικό Σωματείο Α.Τ.Α.Χ.Τ.Ο.Ι., ενώ στην συνέχεια ίδρυσε μαζί με τον αδελφό του Σαράντη, τους Backpack Brothers.',
+    bioEN: ''
+  },
+  {
+    slug: 'zografos',
+    fullNameGR: '',
+    fullNameEN: '',
+    artisticName: 'Zografos',
+    job: 'Καλλιτέχνης - Στιχουργός',
+    infoLinks: {instagram: 'https://www.instagram.com/zografos_vita/'},
+    photo: '',
+    session: 0,
+    time: '',
+    talkCategory: '',
+    talkDescription: '',
+    bioGR:
+      'Ενεργός απ’ τα τέλη της δεκαετίας του 90, ο Ζωγράφος ξεκίνησε να δισκογραφεί ως Solo καλλιτέχνης το 2013 με τον δίσκο ¨Δεν φοβήθηκα να ζήσω ενώ τον Δεκέμβριο του 2022 κυκλοφόρησε το ¨Πυρίτιδα και δυόσμος¨ μαζί με τον Wise Dopes. Έχει στο βιογραφικό του πολλές συνεργασίες με καλλιτέχνες εντός και εκτός του hip hop, ενώ αυτό που τον ξεχωρίζει είναι ο ιδιαίτερος στίχος του, σε συνδυασμό με τις εξίσου ιδιαίτερες παραγωγές του, που συνδέουν την παράδοση με το σήμερα.',
+    bioEN: ''
+  }
+  // {
+  //   slug: '',
+  //   fullNameGR: '',
+  //   fullNameEN: '',
+  //   artisticName: '',
+  //   job: '',
+  //   infoLinks: [''],
+  //   photo: '',
+  //   session: 0,
+  //   time: '',
+  //   talkCategory: '',
+  //   talkDescription: '',
+  //   bioGR: '',
+  //   bioEN: ''
+  // }
+];
---
+++ data/workshops.js
@@ -0,0 +1,23 @@
+// export type ExperienceWorkshop = {
+//   slug: string,
+//   companyName: string,
+//   titleGR: string,
+//   titleEN: string,
+//   descriptionGR: string,
+//   descriptionEN: string,
+//   infoLinks: string[]
+// };
+
+export const EXPERIENCE_WORKSHOPS = [];
+
+// export type FundraisingWorkshop = {
+//   slug: string,
+//   companyName: string,
+//   titleGR: string,
+//   titleEN: string,
+//   descriptionGR: string,
+//   descriptionEN: string,
+//   infoLinks: string[]
+// };
+
+export const FUNDRAISING_WORKSHOPS = [];
