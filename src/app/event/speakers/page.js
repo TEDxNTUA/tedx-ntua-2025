3cdf38e894be2ea02954ed8b713d119381f0b02b
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-04-06 22:01:06 +0300

    feat: create speaker page, add speakers images, speaker grid and card components

---
+++ src/app/event/speakers/page.js
@@ -0,0 +1,9 @@
+import SpeakerGrid from '@components/speakers/SpeakerGrid';
+
+export default function SpeakersListPage() {
+  return (
+    <div className="bg-synelixis-orange/10">
+      <SpeakerGrid />
+    </div>
+  );
+}
