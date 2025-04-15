de29d122f41ad0f384b8cf40a4a6dac83807033a
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-04-15 03:24:37 +0300

    feat: create dummy pages for workshops and side happenings to be announced

---
+++ src/app/event/professional-workshops/page.js
@@ -0,0 +1,10 @@
+import StayTunedPage from '@components/Event/StayTunedPage';
+
+export default function ProfessionalWorkshopsPage() {
+  return <StayTunedPage title="Professional Workshops" />;
+}
+
+export const metadata = {
+  title: 'Professional Workshops - Coming Soon | TEDxNTUA 2025, Synelixis',
+  description: 'Stay tuned for announcements about our Professional Workshops.'
+};
