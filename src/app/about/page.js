5e98036c9cb1005450746066d70bab160fb1bc3b
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-04-16 23:15:54 +0300

    chore: add team page in about/team

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -43,14 +43,6 @@ export default function AboutPage() {
           <AboutInfo textColor="text-synelixis-orange" />
         </div>
       </main>
-
-      {/* This div includes the grid with the team members and their doodles */}
-      <div className="max-w-[2080px] mx-auto flex flex-col lg:w-[90%] w-[80%] px-6 mt-[150px] bg-synelixis-blue">
-        <h2 className="mb-[30px] text-synelixis-yellow text-[2.25rem] lg:text-[4.25rem] font-bold">
-          Meet The Team <span className="text-synelixis-orange">2025</span>
-        </h2>
-        <AboutGrid aboutlist={ABOUTLIST} />
-      </div>
     </div>
   );
 }
--- src/app/about/team/page.js
+++ src/app/about/team/page.js
@@ -1,16 +1,15 @@
 import AboutGrid from '@components/about/AboutGrid';
 import {ABOUTLIST} from '@data';
-{
-  /* This div includes the grid with the team members and their doodles */
-}
 
 function TeamPage() {
   return (
-    <div className="max-w-[2080px] mx-auto flex flex-col lg:w-[90%] w-[80%] bg-synelixis-blue">
-      <h2 className="text-synelixis-yellow text-[2.25rem] lg:text-[4.25rem] font-bold">
-        Meet The Team <span className="text-synelixis-orange">2025</span>
-      </h2>
-      <AboutGrid aboutlist={ABOUTLIST} />
+    <div className="relative min-h-screen overflow-x-hidden overflow-y-visible flex flex-col items-center bg-[url('/background.jpg')] bg-cover bg-center bg-no-repeat bg-fixed">
+      <div className="lg:w-[90%] w-[80%] max-w-[2080px] mx-auto flex flex-col">
+        <h2 className="text-synelixis-yellow text-[2.25rem] lg:text-[4.25rem] font-bold">
+          Meet The Team <span className="text-synelixis-orange">2025</span>
+        </h2>
+        <AboutGrid aboutlist={ABOUTLIST} />
+      </div>
     </div>
   );
 }
