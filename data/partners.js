06ff08aeb988d1d516d62b2548f8b35579912d78
Korina Karatzouni <st2yp01@gmail.com>
2025-04-16 16:21:15 +0300

    Fix layout error in partners page

--- data/partners.js
+++ data/partners.js
@@ -1,195 +1,245 @@
-export const partnersData = [
+import React from 'react';
+import Image from 'next/image';
+import { GlareCard } from '@components/GlareCard';
+
+export const timelineData = [
   {
-    title: 'Platinum Sponsors',
-    content: [
-      {
-        href: 'https://www.acg.edu/',
-        imgSrc: '/Partners/Platinum/ACGLogo.jpg',
-        alt: 'ACG Logo',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full shadow',
+        title: 'Platinum Sponsors',
+        content: (
+          <>
+            <GlareCard href="https://www.acg.edu/">
+              <Image
+                src="/Partners/Platinum/ACGLogo.jpg"
+                alt="ACG Logo"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full shadow"
+              />
+            </GlareCard>
+  
+            <GlareCard href="https://www.archirodon.net/">
+              <Image
+                src="/Partners/Platinum/65years_Archirodon_Fin.jpg"
+                alt="Archirodon 65 Years"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full shadow"
+              />
+            </GlareCard>
+  
+            <GlareCard href="https://www.dei.gr/en/">
+              <Image
+                src="/Partners/Platinum/DEI.jpg"
+                alt="DEI"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full shadow"
+              />
+            </GlareCard>
+            <GlareCard href="https://www.oliverwyman.com/in.html">
+            <Image
+              src="/Partners/Platinum/oliver-wyman-logo.png"
+              alt="Oliver Wyman"
+              width={500}
+              height={500}
+              className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full shadow"
+            />
+          </GlareCard>
+          </>
+        ),
       },
       {
-        href: 'https://www.archirodon.net/',
-        imgSrc: '/Partners/Platinum/65years_Archirodon_Fin.jpg',
-        alt: 'Archirodon 65 Years',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full shadow',
+        title: 'Grand Sponsors',
+        content: (
+          <>
+          <GlareCard href="https://www.space.gr/en">
+            <Image
+              src="/Partners/Grand/space.png"
+              alt="Grand Sponsor 1"
+              width={500}
+              height={500}
+              className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow"
+            />
+          </GlareCard>
+  
+          <GlareCard href="https://www.elpedison.gr/el/">
+            <Image
+              src="/Partners/Grand/elpedison.png"
+              alt="Elpedison"
+              width={500}
+              height={500}
+              className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow"
+            />
+          </GlareCard>
+  
+          <GlareCard href="https://www.thenamaris.com/">
+            <Image
+              src="/Partners/Grand/THENAMARIS.png"
+              alt="thenamaris"
+              width={500}
+              height={500}
+              className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow"
+            />
+          </GlareCard>
+        </>
+          
+        ),
       },
       {
-        href: 'https://www.dei.gr/en/',
-        imgSrc: '/Partners/Platinum/DEI.jpg',
-        alt: 'DEI',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full shadow',
+        title: 'Partners',
+        content: (
+          <>
+            <GlareCard href="https://maps.app.goo.gl/t2PodNBQHvZu9BgCA">
+            <Image
+              src="/Partners/Partners/layers.png"
+              alt="Layers"
+              width={500}
+              height={500}
+              className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow"
+            />
+          </GlareCard>
+          <GlareCard href="https://gr.coca-colahellenic.com/">
+            <Image
+              src="/Partners/Partners/logo3E.jpg"
+              alt="3E"
+              width={500}
+              height={500}
+              className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow"
+            />
+          </GlareCard>
+          <GlareCard href="https://www.frezyderm.gr/?UserAction=True">
+              <Image
+                src="/Partners/Partners/frezyderm.png"
+                alt="Partner 1"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow"
+              />
+            </GlareCard>
+          </>
+        ),
       },
       {
-        href: 'https://www.oliverwyman.com/in.html',
-        imgSrc: '/Partners/Platinum/oliver-wyman-logo.png',
-        alt: 'Oliver Wyman',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full shadow',
-      },
-    ],
-  },
-  {
-    title: 'Grand Sponsors',
-    content: [
-      {
-        href: 'https://www.space.gr/en',
-        imgSrc: '/Partners/Grand/space.png',
-        alt: 'Space',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow',
-      },
-      {
-        href: 'https://www.elpedison.gr/el/',
-        imgSrc: '/Partners/Grand/elpedison.png',
-        alt: 'Elpedison',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow',
-      },
-      {
-        href: 'https://www.thenamaris.com/',
-        imgSrc: '/Partners/Grand/THENAMARIS.png',
-        alt: 'thenamaris',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow',
-      },
-    ],
-  },
-  {
-    title: 'Partners',
-    content: [
-      {
-        href: 'https://maps.app.goo.gl/t2PodNBQHvZu9BgCA',
-        imgSrc: '/Partners/Partners/layers.png',
-        alt: 'Layers',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow',
-      },
-      {
-        href: 'https://gr.coca-colahellenic.com/',
-        imgSrc: '/Partners/Partners/logo3E.jpg',
-        alt: '3E',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow',
-      },
-      {
-        href: 'https://www.frezyderm.gr/?UserAction=True',
-        imgSrc: '/Partners/Partners/frezyderm.png',
-        alt: 'Frezyderm',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full shadow',
-      },
-    ],
-  },
-  {
-    title: 'Supporters',
-    content: [
-      {
-        href: 'https://myredraven.com/?gad_source=1&gclid=CjwKCAjw5PK_BhBBEiwAL7GTPaEnqPEVpxX__Mq2Xx7Z3Pp3f7IBQiiVUElk0wniDdllnsqo13RyvhoCP18QAvD_BwE',
-        imgSrc: '/Partners/Supporters/RedRaven.png',
-        alt: 'RedRaven',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow',
-      },
-      {
-        href: 'https://www.olympos.gr/',
-        imgSrc: '/Partners/Supporters/olympos2.png',
-        alt: 'OLYMPOS',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow',
-      },
-      {
-        href: 'https://www.melissa.gr/en/',
-        imgSrc: '/Partners/Supporters/Melissa.png',
-        alt: 'Melissa',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow',
-      },
-      {
-        href: 'https://megaspileo.gr/?gad_source=1&gclid=CjwKCAjw5PK_BhBBEiwAL7GTPfxmQa_DvfGssV_jD3xHBCoStOyqIJ5F83qwlLmZ5c3w0GQ4u0WvHxoC2wkQAvD_BwE',
-        imgSrc: '/Partners/Supporters/megaspileo.png',
-        alt: 'megaspileo',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow',
-      },
-      {
-        href: 'https://www.nanoudonutshouse.com/',
-        imgSrc: '/Partners/Supporters/nanou.png',
-        alt: 'nanou',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow',
+        title: 'Supporters',
+        content: (
+          <>
+            <GlareCard href="https://myredraven.com/?gad_source=1&gclid=CjwKCAjw5PK_BhBBEiwAL7GTPaEnqPEVpxX__Mq2Xx7Z3Pp3f7IBQiiVUElk0wniDdllnsqo13RyvhoCP18QAvD_BwE">
+              <Image
+                src="/Partners/Supporters/RedRaven.png"
+                alt="RedRaven"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow"
+              />
+            </GlareCard>
+  
+            <GlareCard href="https://www.olympos.gr/">
+              <Image
+                src="/Partners/Supporters/olympos2.png"
+                alt="OLYMPOS"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow"
+              />
+            </GlareCard>
+  
+            <GlareCard href="https://www.melissa.gr/en/">
+              <Image
+                src="/Partners/Supporters/Melissa.png"
+                alt="Melissa"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow"
+              />
+            </GlareCard>
+  
+            <GlareCard href="https://megaspileo.gr/?gad_source=1&gclid=CjwKCAjw5PK_BhBBEiwAL7GTPfxmQa_DvfGssV_jD3xHBCoStOyqIJ5F83qwlLmZ5c3w0GQ4u0WvHxoC2wkQAvD_BwE">
+              <Image
+                src="/Partners/Supporters/megaspileo.png"
+                alt="megaspileo"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow"
+              />
+            </GlareCard>
+  
+            <GlareCard href="https://www.nanoudonutshouse.com/">
+              <Image
+                src="/Partners/Supporters/nanou.png"
+                alt="nanou"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow"
+              />
+            </GlareCard>
+  
+            <GlareCard href="https://www.ikea.gr/">
+              <Image
+                src="/Partners/Supporters/IKEA2.png"
+                alt="IKEA"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow"
+              />
+            </GlareCard>
+  
+            <GlareCard href="https://www.deepsea.ai/">
+              <Image
+                src="/Partners/Supporters/deepsealogo.png"
+                alt="deepsealogo"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow"
+              />
+            </GlareCard>
+            
+            <GlareCard href="https://www.ahmadtea.com/">
+              <Image
+                src="/Partners/Supporters/AhmadTea.jpg"
+                alt="AhmadTea"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow"
+              />
+            </GlareCard>
+  
+            <GlareCard href="https://www.flexopack.com/en/">
+              <Image
+                src="/Partners/Supporters/flexopack.png"
+                alt="Flexopack"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow"
+              />
+            </GlareCard>
+  
+            <GlareCard href="https://kanellakis-sa.gr/">
+              <Image
+                src="/Partners/Supporters/kanellakis.png"
+                alt="kanellakis"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow"
+              />
+            </GlareCard>
+  
+          </>
+        ),
       },
       {
-        href: 'https://www.ikea.gr/',
-        imgSrc: '/Partners/Supporters/IKEA2.png',
-        alt: 'IKEA',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow',
+        title: 'Exclusive Air Transport Sponsor',
+        content: (
+          <>
+            <GlareCard href="https://en.aegeanair.com/">
+              <Image
+                src="/Partners/Supporters/aegean.png"
+                alt="Aegean"
+                width={500}
+                height={500}
+                className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full shadow"
+              />
+            </GlareCard>
+          </>
+        ),
       },
-      {
-        href: 'https://www.deepsea.ai/',
-        imgSrc: '/Partners/Supporters/deepsealogo.png',
-        alt: 'deepsealogo',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow',
-      },
-      {
-        href: 'https://www.ahmadtea.com/',
-        imgSrc: '/Partners/Supporters/AhmadTea.jpg',
-        alt: 'AhmadTea',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow',
-      },
-      {
-        href: 'https://www.flexopack.com/en/',
-        imgSrc: '/Partners/Supporters/flexopack.png',
-        alt: 'Flexopack',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow',
-      },
-      {
-        href: 'https://kanellakis-sa.gr/',
-        imgSrc: '/Partners/Supporters/kanellakis.png',
-        alt: 'kanellakis',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full shadow',
-      },
-    ],
-  },
-  {
-    title: 'Exclusive Air Transport Sponsor',
-    content: [
-      {
-        href: 'https://en.aegeanair.com/',
-        imgSrc: '/Partners/Supporters/aegean.png',
-        alt: 'Aegean',
-        width: 500,
-        height: 500,
-        className: 'rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full shadow',
-      },
-    ],
-  },
-];
+    ];
