514e77cc59de1769172452c11b2636ab1c624d94
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-04-17 03:18:25 +0300

    fix: test image path for deploy

--- src/components/Event/EventCard.js
+++ src/components/Event/EventCard.js
@@ -19,7 +19,7 @@ function EventCard({host, type}) {
   switch (type) {
     case 'speaker':
       linkUrl = `/event/speakers/${slug}`;
-      imageUrl = `/event/speakers/${slug}.jpg`;
+      imageUrl = `./event/speakers/${slug}.jpg`;
       displayName = host.fullNameEN || host.artisticName;
       secondaryInfo = host.jobEN;
       altText = `Picture of ${type}: ${displayName}`;
@@ -28,7 +28,7 @@ function EventCard({host, type}) {
       break;
     case 'performance':
       linkUrl = `/event/performances/${slug}`;
-      imageUrl = `/event/performances/${slug}.jpg`;
+      imageUrl = `./event/performances/${slug}.jpg`;
       displayName = host.artisticNameEN || host.fullNameEN;
       secondaryInfo = host.performanceTypeEN;
       altText = `Image for ${type}: ${displayName}`;
@@ -37,7 +37,7 @@ function EventCard({host, type}) {
       break;
     case 'side-happening':
       linkUrl = `/event/side-happenings/${slug}`;
-      imageUrl = `/event/side-happenings/${slug}.jpg`;
+      imageUrl = `./event/side-happenings/${slug}.jpg`;
       displayName = host.name;
       secondaryInfo = host.description || '';
       altText = `Picture of ${type}: ${displayName}`;
@@ -47,7 +47,7 @@ function EventCard({host, type}) {
     case 'fundraising-workshop':
       const workshopBasePath = `${type}s`;
       linkUrl = `/event/${workshopBasePath}s/${slug}`;
-      imageUrl = `/event/${workshopBasePath}s/${slug}.jpg`;
+      imageUrl = `./event/${workshopBasePath}s/${slug}.jpg`;
       displayName = host.companyName;
       secondaryInfo = host.titleEN || host.titleGR;
       altText = `Picture of ${type}: ${displayName}`;
