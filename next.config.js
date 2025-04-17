21766e286c4bc44db2738eb0b874a74aae9a27f5
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-04-17 03:30:17 +0300

    chore: change next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'assets.aceternity.com',
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig; 
+  reactStrictMode: false,
+  output: 'export'
+};
+
+module.exports = nextConfig;
