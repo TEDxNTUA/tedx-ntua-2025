6fe9c9905d69718b5b261df62eb5ff9dd15a245b
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-04-17 18:16:22 +0300

    fix bg in about page

--- src/app/about/team/page.js
+++ src/app/about/team/page.js
@@ -9,9 +9,13 @@ export const metadata = {
 function TeamPage() {
   return (
     <div
-      className="relative min-h-screen overflow-x-hidden overflow-y-visible flex flex-col items-center bg-cover bg-center bg-no-repeat bg-fixed"
+      className="relative min-h-screen overflow-x-hidden overflow-y-visible flex flex-col items-center"
       style={{
-        backgroundImage: `url('/background.jpg')`
+        backgroundImage: `url('/background.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed'
       }}
     >
       <div className="lg:w-[90%] w-[80%] max-w-[2080px] mx-auto pt-20">
