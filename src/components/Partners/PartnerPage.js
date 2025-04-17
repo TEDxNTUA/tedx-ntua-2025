81479952e23cce30dee30dfaef34853ef4dde6b8
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-04-17 06:49:32 +0300

    feat: add animations for partners page and give it some life xD

---
+++ src/components/Partners/PartnerPage.js
@@ -0,0 +1,321 @@
+'use client';
+
+import React, {useEffect} from 'react';
+import gsap from 'gsap';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import Timeline from '@components/Timeline';
+
+gsap.registerPlugin(ScrollTrigger);
+
+function PartnersPageComponent() {
+  useEffect(() => {
+    gsap.utils.toArray('.sponsor img').forEach(img => {
+      gsap.from(img, {
+        scrollTrigger: {
+          trigger: img,
+          start: 'top 80%',
+          end: 'bottom 60%',
+          toggleActions: 'play none none reverse'
+        },
+        opacity: 0,
+        y: 50,
+        duration: 1,
+        ease: 'power2.out'
+      });
+    });
+  }, []);
+
+  const timelineData = [
+    {
+      title: 'Platinum Sponsors',
+      content: (
+        <>
+          <img
+            src="/partners/platinum/the-american-college-of-greece.webp"
+            alt="ACG Logo"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/platinum/archidoron.webp"
+            alt="Archirodon 65 Years"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/platinum/dei.webp"
+            alt="DEI"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/platinum/oliver-wyman-1.webp"
+            alt="Oliver Wyman"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+        </>
+      )
+    },
+    {
+      title: 'Grand Sponsors',
+      content: (
+        <>
+          <img
+            src="/partners/grand/uniperfect.webp"
+            alt="uniperfect"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/grand/elpedison.webp"
+            alt="elpedison"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/grand/thenamaris.webp"
+            alt="thenamaris"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/grand/space.webp"
+            alt="space"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+        </>
+      )
+    },
+    {
+      title: 'Partners',
+      content: (
+        <>
+          <img
+            src="/partners/partners/coca-cola.webp"
+            alt="Coca Cola 3E"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full lg:grayscale-[0.65] lg:hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/partners/frenzyderm.webp"
+            alt="Frenzyderm"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/partners/red-bull.webp"
+            alt="Red Bull"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/partners/papoutsanis.webp"
+            alt="Partner 1"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          {/* <img
+            src="/partners/partners/q-project.webp"
+            alt="Partner 1"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full grayscale-[0.65] hover:grayscale-0"
+          /> */}
+        </>
+      )
+    },
+    {
+      title: 'Supporters',
+      content: (
+        <>
+          <img
+            src="/partners/supporters/olympos.webp"
+            alt="OLYMPOS"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/melissa-1.webp"
+            alt="Melissa"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/mega-spileo.webp"
+            alt="mega-spileo"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/nanou.webp"
+            alt="nanou"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/ikea.webp"
+            alt="IKEA"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/deepsea-1.webp"
+            alt="deepsealogo"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+          <img
+            src="/partners/supporters/ahmad-tea.webp"
+            alt="ahmad-tea"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/atflya.webp"
+            alt="atflya"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/cavino.webp"
+            alt="cavino"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/colgate-1.webp"
+            alt="Colgate"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/flexopack.webp"
+            alt="Flexopack"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/kanellakis.webp"
+            alt="kanellakis"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.9] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/red-raven.webp"
+            alt="Red Raven"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.9] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/rodopaki.webp"
+            alt="Rodopaki"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.9] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/symetal.webp"
+            alt="Symetal"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.9] hover:grayscale-0"
+          />
+
+          <img
+            src="/partners/supporters/tefaco.webp"
+            alt="Tefaco"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-14 md:h-24 lg:h-28 w-full grayscale-[0.9] hover:grayscale-0"
+          />
+        </>
+      )
+    },
+    {
+      title: 'Exclusive Air Transport Sponsor',
+      content: (
+        <>
+          <img
+            src="/partners/exclusive-air/aegean.webp"
+            alt="Aegean"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full grayscale hover:grayscale-0"
+          />
+        </>
+      )
+    },
+    {
+      title: 'Exclusive Burger Provider',
+      content: (
+        <>
+          <img
+            src="/partners/exclusive-burger/layers.webp"
+            alt="Aegean"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full grayscale-[0.65] hover:grayscale-0"
+          />
+        </>
+      )
+    }
+  ];
+
+  return (
+    <section>
+      <Timeline data={timelineData} />
+    </section>
+  );
+}
+
+export default PartnersPageComponent;
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -25,8 +25,7 @@ const Timeline = ({data}) => {
 
   return (
     <>
-      <h2 className="text-lg text-synelixis-yellow">TedxNTUA2025 Sponsors</h2>
-      <div className="w-full font-sans md:px-10" ref={containerRef}>
+      <div className="w-full md:px-10 " ref={containerRef}>
         <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
           {data.map((item, index) => (
             <div key={index} className="flex justify-start pt-10 md:pt-40 md:gap-10">
@@ -34,13 +33,13 @@ const Timeline = ({data}) => {
                 <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
                   <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
                 </div>
-                <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-synelixis-blue">
+                <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-white">
                   {item.title}
                 </h3>
               </div>
 
               <div className="relative pl-20 pr-4 md:pl-4 w-full">
-                <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-synelixis-blue">
+                <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-white">
                   {item.title}
                 </h3>
 
