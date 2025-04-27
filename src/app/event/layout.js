ef4713e14fbcba78e22d0ac6fa48e01756433906
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-04-27 20:26:17 +0300

    fix: make event subnavigation hidden in small screens

--- src/app/event/layout.js
+++ src/app/event/layout.js
@@ -2,8 +2,10 @@ import EventFilterNav from '@components/EventFilterNav'; // Adjust path if neede
 
 export default function EventLayout({children}) {
   return (
-    <div>
-      <EventFilterNav />
+    <div className="pt-6 lg:pt-0">
+      <div className="hidden lg:block">
+        <EventFilterNav />
+      </div>
       <main>{children}</main>
     </div>
   );
